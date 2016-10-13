71cb29ebc61eb0653e2376ff722a320b96a034d8
LiJunTian <1661370619@qq.com>
2016-10-13 21:03:11 +0800

    生成实体类

---
+++ app/src/main/java/cn/ucai/fulicenter/bean/GoodsDetailsBean.java
@@ -0,0 +1,192 @@
+package cn.ucai.fulicenter.bean;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/10/13 0013.
+ */
+public class GoodsDetailsBean {
+
+    /**
+     * id : 280
+     * goodsId : 7677
+     * catId : 291
+     * goodsName : 双层分格饭盒 绿色
+     * goodsEnglishName : Monbento
+     * goodsBrief : PP食品级材质，轻巧、易清洗、蠕变性小，不易变形，可置于微波炉加热，可方巾洗碗机清洗。双层色彩可以随意组合，轻巧方便。
+     * shopPrice : ￥253
+     * currencyPrice : ￥293
+     * promotePrice : ￥0
+     * rankPrice : ￥293
+     * isPromote : false
+     * goodsThumb : 201509/thumb_img/7677_thumb_G_1442391216339.png
+     * goodsImg : 201509/thumb_img/7677_thumb_G_1442391216339.png
+     * addTime : 1442419200000
+     * shareUrl : http://m.fulishe.com/item/7677
+     * properties : [{"id":9529,"goodsId":0,"colorId":7,"colorName":"白色","colorCode":"#ffffff","colorImg":"","colorUrl":"https://detail.tmall.com/item.htm?spm=a1z10.5-b.w4011-3609973698.66.6PtkVY&id=520971761592&rn=5ddf7aff64dbe1a24da0eaf7409e3389&abbucket=15&skuId=3104519239252","albums":[{"pid":7677,"imgId":28296,"imgUrl":"201509/goods_img/7677_P_1442391216432.png","thumbUrl":"no_picture.gif"},{"pid":7677,"imgId":28297,"imgUrl":"201509/goods_img/7677_P_1442391216215.png","thumbUrl":"no_picture.gif"},{"pid":7677,"imgId":28298,"imgUrl":"201509/goods_img/7677_P_1442391216692.png","thumbUrl":"no_picture.gif"},{"pid":7677,"imgId":28299,"imgUrl":"201509/goods_img/7677_P_1442391216316.png","thumbUrl":"no_picture.gif"}]}]
+     * promote : false
+     */
+
+    private int id;
+
+    private int goodsId;
+
+    private int catId;
+
+    private String goodsName;
+
+    private String goodsEnglishName;
+
+    private String goodsBrief;
+
+    private String shopPrice;
+
+    private String currencyPrice;
+
+    private String promotePrice;
+
+    private String rankPrice;
+
+    private boolean isPromote;
+
+    private String goodsThumb;
+
+    private String goodsImg;
+
+    private int addTime;
+
+    private String shareUrl;
+
+    private List<PropertiesBean> properties ;
+
+    private boolean promote;
+
+    public GoodsDetailsBean() {
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId(){
+        return this.id;
+    }
+    public void setGoodsId(int goodsId){
+        this.goodsId = goodsId;
+    }
+    public int getGoodsId(){
+        return this.goodsId;
+    }
+    public void setCatId(int catId){
+        this.catId = catId;
+    }
+    public int getCatId(){
+        return this.catId;
+    }
+    public void setGoodsName(String goodsName){
+        this.goodsName = goodsName;
+    }
+    public String getGoodsName(){
+        return this.goodsName;
+    }
+    public void setGoodsEnglishName(String goodsEnglishName){
+        this.goodsEnglishName = goodsEnglishName;
+    }
+    public String getGoodsEnglishName(){
+        return this.goodsEnglishName;
+    }
+    public void setGoodsBrief(String goodsBrief){
+        this.goodsBrief = goodsBrief;
+    }
+    public String getGoodsBrief(){
+        return this.goodsBrief;
+    }
+    public void setShopPrice(String shopPrice){
+        this.shopPrice = shopPrice;
+    }
+    public String getShopPrice(){
+        return this.shopPrice;
+    }
+    public void setCurrencyPrice(String currencyPrice){
+        this.currencyPrice = currencyPrice;
+    }
+    public String getCurrencyPrice(){
+        return this.currencyPrice;
+    }
+    public void setPromotePrice(String promotePrice){
+        this.promotePrice = promotePrice;
+    }
+    public String getPromotePrice(){
+        return this.promotePrice;
+    }
+    public void setRankPrice(String rankPrice){
+        this.rankPrice = rankPrice;
+    }
+    public String getRankPrice(){
+        return this.rankPrice;
+    }
+    public void setIsPromote(boolean isPromote){
+        this.isPromote = isPromote;
+    }
+    public boolean getIsPromote(){
+        return this.isPromote;
+    }
+    public void setGoodsThumb(String goodsThumb){
+        this.goodsThumb = goodsThumb;
+    }
+    public String getGoodsThumb(){
+        return this.goodsThumb;
+    }
+    public void setGoodsImg(String goodsImg){
+        this.goodsImg = goodsImg;
+    }
+    public String getGoodsImg(){
+        return this.goodsImg;
+    }
+    public void setAddTime(int addTime){
+        this.addTime = addTime;
+    }
+    public int getAddTime(){
+        return this.addTime;
+    }
+    public void setShareUrl(String shareUrl){
+        this.shareUrl = shareUrl;
+    }
+    public String getShareUrl(){
+        return this.shareUrl;
+    }
+    public void setProperties(List<PropertiesBean> properties){
+        this.properties = properties;
+    }
+    public List<PropertiesBean> getProperties(){
+        return this.properties;
+    }
+    public void setPromote(boolean promote){
+        this.promote = promote;
+    }
+    public boolean getPromote(){
+        return this.promote;
+    }
+
+    @Override
+    public String toString() {
+        return "GoodsDetailsBean{" +
+                "id=" + id +
+                ", goodsId=" + goodsId +
+                ", catId=" + catId +
+                ", goodsName='" + goodsName + '\'' +
+                ", goodsEnglishName='" + goodsEnglishName + '\'' +
+                ", goodsBrief='" + goodsBrief + '\'' +
+                ", shopPrice='" + shopPrice + '\'' +
+                ", currencyPrice='" + currencyPrice + '\'' +
+                ", promotePrice='" + promotePrice + '\'' +
+                ", rankPrice='" + rankPrice + '\'' +
+                ", isPromote=" + isPromote +
+                ", goodsThumb='" + goodsThumb + '\'' +
+                ", goodsImg='" + goodsImg + '\'' +
+                ", addTime=" + addTime +
+                ", shareUrl='" + shareUrl + '\'' +
+                ", properties=" + properties +
+                ", promote=" + promote +
+                '}';
+    }
+}
---
+++ app/src/main/java/cn/ucai/fulicenter/bean/PropertiesBean.java
@@ -0,0 +1,102 @@
+package cn.ucai.fulicenter.bean;
+
+import java.util.List;
+import cn.ucai.fulicenter.bean.AlbumsBean;
+/**
+ * Created by Administrator on 2016/10/13 0013.
+ */
+public class PropertiesBean {
+
+    /**
+     * id : 8514
+     * goodsId : 0
+     * colorId : 4
+     * colorName : 绿色
+     * colorCode : #59d85c
+     * colorImg : 201309/1380064997570506166.jpg
+     * colorUrl : https://cn.shopbop.com/alexa-chung-loretta-romper-ag/vp/v=1/1573999972.htm?fm=search-shopbysize&os=false
+     * albums : [{"pid":6936,"imgId":26104,"imgUrl":"201508/goods_img/6936_P_1439535131675.png","thumbUrl":"no_picture.gif"}]
+     */
+
+    private int id;
+
+    private int goodsId;
+
+    private int colorId;
+
+    private String colorName;
+
+    private String colorCode;
+
+    private String colorImg;
+
+    private String colorUrl;
+
+    private List<AlbumsBean> albumsBean ;
+
+    public PropertiesBean() {
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId(){
+        return this.id;
+    }
+    public void setGoodsId(int goodsId){
+        this.goodsId = goodsId;
+    }
+    public int getGoodsId(){
+        return this.goodsId;
+    }
+    public void setColorId(int colorId){
+        this.colorId = colorId;
+    }
+    public int getColorId(){
+        return this.colorId;
+    }
+    public void setColorName(String colorName){
+        this.colorName = colorName;
+    }
+    public String getColorName(){
+        return this.colorName;
+    }
+    public void setColorCode(String colorCode){
+        this.colorCode = colorCode;
+    }
+    public String getColorCode(){
+        return this.colorCode;
+    }
+    public void setColorImg(String colorImg){
+        this.colorImg = colorImg;
+    }
+    public String getColorImg(){
+        return this.colorImg;
+    }
+    public void setColorUrl(String colorUrl){
+        this.colorUrl = colorUrl;
+    }
+    public String getColorUrl(){
+        return this.colorUrl;
+    }
+    public void setAlbums(List<AlbumsBean> albums){
+        this.albumsBean = albumsBean;
+    }
+    public List<AlbumsBean> getAlbums(){
+        return this.albumsBean;
+    }
+
+    @Override
+    public String toString() {
+        return "PropertiesBean{" +
+                "id=" + id +
+                ", goodsId=" + goodsId +
+                ", colorId=" + colorId +
+                ", colorName='" + colorName + '\'' +
+                ", colorCode='" + colorCode + '\'' +
+                ", colorImg='" + colorImg + '\'' +
+                ", colorUrl='" + colorUrl + '\'' +
+                ", albumsBean=" + albumsBean +
+                '}';
+    }
+}
