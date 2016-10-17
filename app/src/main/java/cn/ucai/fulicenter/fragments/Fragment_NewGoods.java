517772de0e36e1f52ccd675362791bfeb1f30be1
LiJunTian <1661370619@qq.com>
2016-10-17 15:37:08 +0800

    添加显示图片的工具类

--- app/src/main/java/cn/ucai/fulicenter/fragments/Fragment_NewGoods.java
+++ app/src/main/java/cn/ucai/fulicenter/fragments/Fragment_NewGoods.java
@@ -3,18 +3,45 @@ package cn.ucai.fulicenter.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.activity.MainActivity;
+import cn.ucai.fulicenter.adapter.GoodsAdapter;
+import cn.ucai.fulicenter.adapter.GoodsAdapter;
+import cn.ucai.fulicenter.bean.NewGoodsBean;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Fragment_NewGoods extends Fragment {
+    static int ACTION_DOWNLOAD = 0;
+    static int ACTION_PULL = 1;
+    static int ACTION_PULLDONW = 2;
+    final int CART_ID = 0;
+    static int PAGE_ID = 1;
+    static int PAGE_SIZE = 10;
 
-
+    @BindView(R.id.tv_refresh)
+    TextView tvRefresh;
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @BindView(R.id.srl)
+    SwipeRefreshLayout srl;
+    MainActivity mContext;
+    ArrayList<NewGoodsBean> list;
+    GoodsAdapter mAdapter;
     public Fragment_NewGoods() {
         // Required empty public constructor
     }
@@ -24,7 +51,24 @@ public class Fragment_NewGoods extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment__new_goods, container, false);
+        View view = inflater.inflate(R.layout.fragment_fragment__new_goods, container, false);
+        ButterKnife.bind(this, view);
+        initView();
+        return view;
+    }
+
+    private void initView() {
+        mAdapter = new GoodsAdapter(mContext,list);
+        srl.setColorSchemeColors(
+                getResources().getColor(R.color.google_blue),
+                getResources().getColor(R.color.google_green),
+                getResources().getColor(R.color.red),
+                getResources().getColor(R.color.google_yellow)
+        );
+        GridLayoutManager glm = new GridLayoutManager(mContext, I.COLUM_NUM);
+        recyclerView.setLayoutManager(glm);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(mAdapter);
     }
 
 }
