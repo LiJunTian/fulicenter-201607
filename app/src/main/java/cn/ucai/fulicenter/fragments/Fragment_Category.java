01fe1973d03e8956296160d31cdd64a6fd1f1f00
LiJunTian <1661370619@qq.com>
2016-10-17 11:43:23 +0800

    添加butterknife注解，实现底部菜单互斥，新建5个fragment (2)

---
+++ app/src/main/java/cn/ucai/fulicenter/fragments/Fragment_Category.java
@@ -0,0 +1,30 @@
+package cn.ucai.fulicenter.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.ucai.fulicenter.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment_Category extends Fragment {
+
+
+    public Fragment_Category() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment__category, container, false);
+    }
+
+}
