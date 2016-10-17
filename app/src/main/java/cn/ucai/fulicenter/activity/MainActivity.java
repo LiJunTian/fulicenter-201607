09524e191283785fde44766aaed0490e919b6a13
LiJunTian <1661370619@qq.com>
2016-10-17 11:37:48 +0800

    添加butterknife注解，实现底部菜单互斥，新建5个fragment

--- app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
@@ -1,15 +1,104 @@
 package cn.ucai.fulicenter.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.ucai.fulicenter.L;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.fragments.Fragment_Boutique;
+import cn.ucai.fulicenter.fragments.Fragment_Cart;
+import cn.ucai.fulicenter.fragments.Fragment_Category;
+import cn.ucai.fulicenter.fragments.Fragment_NewGoods;
+import cn.ucai.fulicenter.fragments.Fragment_PersonCenter;
 
 public class MainActivity extends AppCompatActivity {
+    View mFragmentLayout;
+    List<Fragment> list = new ArrayList<>();
+    Fragment_NewGoods mFragment_newGoods;
+    Fragment_Boutique mFragment_Boutique;
+    Fragment_Category mFragment_Category;
+    Fragment_Cart mFragment_Cart;
+    Fragment_PersonCenter mFragment_PersonCenter;
+
+    RadioGroup mRadioGroup;
+    int index;
+    int currentIndex = 0;
+
+    RadioButton[] rbs;
+    @BindView(R.id.main_rbNewGoods)
+    RadioButton mainRbNewGoods;
+    @BindView(R.id.main_rbBoutique)
+    RadioButton mainRbBoutique;
+    @BindView(R.id.main_rbCategory)
+    RadioButton mainRbCategory;
+    @BindView(R.id.main_rbCart)
+    RadioButton mainRbCart;
+    @BindView(R.id.main_tvCount)
+    TextView mainTvCount;
+    @BindView(R.id.main_rbPersonalCenter)
+    RadioButton mainRbPersonalCenter;
+    @BindView(R.id.main_RadioGroup)
+    LinearLayout mainRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initView();
+    }
+
+    private void initView() {
+        rbs = new RadioButton[5];
+        rbs[0] = mainRbNewGoods;
+        rbs[1] = mainRbBoutique;
+        rbs[2] = mainRbCategory;
+        rbs[3] = mainRbCart;
+        rbs[4] = mainRbPersonalCenter;
+    }
+    public void onCheckedChange(View view){
+        L.i("onCheckedChange---------");
+        switch(view.getId()){
+            case R.id.main_rbNewGoods:
+                index = 0;
+                break;
+            case R.id.main_rbBoutique:
+                index = 1;
+                break;
+            case R.id.main_rbCategory:
+                index = 2;
+                break;
+            case R.id.main_rbCart:
+                index = 3;
+                break;
+            case R.id.main_rbPersonalCenter:
+                index = 4;
+                break;
+        }
+        setRadioButtonStatus();
+    }
+
+    private void setRadioButtonStatus() {
+        for(int i = 0;i<rbs.length;i++){
+            if(i == index){
+                rbs[i].setChecked(true);
+            }else{
+                rbs[i].setChecked(false);
+            }
+        }
     }
 }
