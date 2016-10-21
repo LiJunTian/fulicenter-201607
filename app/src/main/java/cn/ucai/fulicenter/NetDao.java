129487ff2a516fa0ba013f8a37c1eb82f82c655a
LiJunTian <1661370619@qq.com>
2016-10-21 09:03:03 +0800

    在NetDao中添加下载group和child，childDetails的方法

--- app/src/main/java/cn/ucai/fulicenter/NetDao.java
+++ app/src/main/java/cn/ucai/fulicenter/NetDao.java
@@ -3,6 +3,8 @@ package cn.ucai.fulicenter;
 import android.content.Context;
 
 import cn.ucai.fulicenter.bean.BoutiqueBean;
+import cn.ucai.fulicenter.bean.CategoryChildBean;
+import cn.ucai.fulicenter.bean.CategoryGroupBean;
 import cn.ucai.fulicenter.bean.GoodsDetailsBean;
 import cn.ucai.fulicenter.bean.NewGoodsBean;
 import cn.ucai.fulicenter.net.OkHttpUtils;
@@ -34,6 +36,31 @@ public class NetDao {
         OkHttpUtils utils = new OkHttpUtils(context);
         utils.setRequestUrl(I.REQUEST_FIND_BOUTIQUES)
                 .targetClass(BoutiqueBean[].class)
+                .execute(listener);
+    }
+
+    public static void downloadCategoryChild(Context context, int parenId, OkHttpUtils.OnCompleteListener<CategoryChildBean[]> listener){
+        OkHttpUtils utils = new OkHttpUtils(context);
+        utils.setRequestUrl(I.REQUEST_FIND_CATEGORY_CHILDREN)
+                .addParam(I.CategoryChild.PARENT_ID,""+parenId)
+                .targetClass(CategoryChildBean[].class)
+                .execute(listener);
+    }
+
+    public static void downloadCategoryGroup(Context context, OkHttpUtils.OnCompleteListener<CategoryGroupBean[]> listener){
+        OkHttpUtils<CategoryGroupBean[]> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_FIND_CATEGORY_GROUP)
+                .targetClass(CategoryGroupBean[].class)
+                .execute(listener);
+    }
+
+    public static void downloadCategoryChildDetails(Context context,int cartId,int pageId, OkHttpUtils.OnCompleteListener<NewGoodsBean[]> listener){
+        OkHttpUtils<NewGoodsBean[]> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_FIND_GOODS_DETAILS)
+                .addParam(I.GoodsDetails.KEY_CAT_ID,String.valueOf(cartId))
+                .addParam(I.PAGE_ID,String.valueOf(pageId))
+                .addParam(I.PAGE_SIZE,String.valueOf(I.PAGE_SIZE_DEFAULT))
+                .targetClass(NewGoodsBean[].class)
                 .execute(listener);
     }
 }
