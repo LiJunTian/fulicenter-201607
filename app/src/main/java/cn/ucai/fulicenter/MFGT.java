a87209472dadc7ca79f0fbc4783b6e9c0c3d70ed
LiJunTian <1661370619@qq.com>
2016-10-22 09:27:12 +0800

    在gotoCategoryChildActivity方法中中添加childList参数

--- app/src/main/java/cn/ucai/fulicenter/MFGT.java
+++ app/src/main/java/cn/ucai/fulicenter/MFGT.java
@@ -3,11 +3,19 @@ package cn.ucai.fulicenter;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 
 import cn.ucai.fulicenter.activity.BoutiqueDetailActivity;
 import cn.ucai.fulicenter.activity.CategoryChildActivity;
 import cn.ucai.fulicenter.activity.GoodsDetailsActivity;
+import cn.ucai.fulicenter.activity.LoginActivity;
 import cn.ucai.fulicenter.activity.MainActivity;
+import cn.ucai.fulicenter.activity.RegisterActivity;
+import cn.ucai.fulicenter.bean.CategoryChildBean;
+import cn.ucai.fulicenter.fragments.Fragment_PersonCenter;
 
 /**
  * Created by Administrator on 2016/10/14 0014.
@@ -46,11 +54,26 @@ public class MFGT {//从哪来到哪去move from go to
         startActivity(context,intent);
     }
 
-    public static void gotoCategoryChildActivity(Context context,int catId,String title){
+    public static void gotoCategoryChildActivity(Context context, int catId, String title, ArrayList<CategoryChildBean> list){
         Intent intent = new Intent();
         intent.setClass(context, CategoryChildActivity.class);
         intent.putExtra(I.CategoryChild.CAT_ID,catId);
         intent.putExtra(I.CategoryGroup.NAME,title);
+        intent.putExtra(I.CategoryChild.ID,list);
         startActivity(context,intent);
     }
+
+    public static void gotoLoginActivity(Context context){
+        Intent intent = new Intent();
+        intent.setClass(context, LoginActivity.class);
+        startActivity(context,intent);
+    }
+
+    public static void gotoPersonCenter(Context context,String userName){
+        Intent intent = new Intent();
+        intent.putExtra(I.User.USER_NAME,userName);
+        intent.setClass(context,MainActivity.class);
+        startActivity(context,intent);
+    }
+
 }
