3fe11abe835bd7790fc8a150bf242d37840d6435
LiJunTian <1661370619@qq.com>
2016-10-26 21:10:54 +0800

    修复更新头像和昵称bug

--- app/src/main/java/cn/ucai/fulicenter/activity/PersonalActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/PersonalActivity.java
@@ -1,14 +1,26 @@
 package cn.ucai.fulicenter.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.io.File;
 
 import butterknife.BindView;
@@ -19,6 +31,7 @@ import cn.ucai.fulicenter.adapter.FuLiCenterApplication;
 import cn.ucai.fulicenter.bean.Result;
 import cn.ucai.fulicenter.bean.User;
 import cn.ucai.fulicenter.dao.SharePrefrenceUtils;
+import cn.ucai.fulicenter.dao.UserDao;
 import cn.ucai.fulicenter.net.ImageLoader;
 import cn.ucai.fulicenter.net.NetDao;
 import cn.ucai.fulicenter.net.OkHttpUtils;
@@ -52,6 +65,11 @@ public class PersonalActivity extends BaseActivity {
     ImageView ivPersonalQrcode;
     @BindView(R.id.ll_avatar_layout_set)
     LinearLayout llAvatarLayoutSet;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -60,6 +78,9 @@ public class PersonalActivity extends BaseActivity {
         ButterKnife.bind(this);
         mContext = this;
         super.onCreate(savedInstanceState);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -99,23 +120,79 @@ public class PersonalActivity extends BaseActivity {
 
     @OnClick(R.id.iv_personal_setNick)
     public void onSetNick() {
-        startActivityForResult(new Intent(this, RevisePersonalActivity.class), I.REQUEST_CODE_UPDATE_NICK);
+//        startActivityForResult(new Intent(this, RevisePersonalActivity.class), I.REQUEST_CODE_UPDATE_NICK);
+        final EditText editText = new EditText(this);
+        new AlertDialog.Builder(this)
+                .setTitle("修改昵称")
+                .setView(editText)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        if (user != null) {
+                            String nick = editText.getText().toString();
+                            if (nick.equals(user.getMuserNick())) {
+                                CommonUtils.showLongToast("昵称与原昵称一致");
+                            } else if (TextUtils.isEmpty(nick)) {
+                                CommonUtils.showLongToast("昵称为空");
+                            } else {
+                                updateNick(user.getMuserName(), nick);
+                            }
+                        }
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("取消",null)
+                .show();
+    }
+
+    private void updateNick(String userName, String nick) {
+        NetDao.updateNick(mContext, userName, nick, new OkHttpUtils.OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                Result result = ResultUtils.getResultFromJson(s, User.class);
+                if (result != null) {
+                    if (result.isRetMsg()) {
+//                        L.e(TAG,"result="+result);
+                        User user = (User) result.getRetData();
+//                        L.e(TAG,"user="+user);
+                        UserDao dao = new UserDao(mContext);
+                        boolean isSuccess = dao.updateUser(user);
+                        if (isSuccess) {
+                            FuLiCenterApplication.setUser(user);
+                            initData();
+//                            setResult(RESULT_OK);
+//                            MFGT.finish((Activity) mContext);
+                        }
+                        CommonUtils.showLongToast("昵称更新成功");
+                    } else if (result.getRetCode() == I.MSG_USER_SAME_NICK) {
+                        CommonUtils.showLongToast("昵称未修改");
+                    } else {
+                        CommonUtils.showLongToast("昵称修改失败");
+                    }
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 //        if(resultCode==RESULT_OK&&requestCode == I.REQUEST_CODE_UPDATE_NICK){
-            if (resultCode!=RESULT_OK) {
-                return;
-            }
-            osal.setAvatar(requestCode, data, ivPersonalAvatar);
-            if (requestCode == I.REQUEST_CODE_UPDATE_NICK) {
-                initData();
-            }
-            if (requestCode == OnSetAvatarListener.REQUEST_CROP_PHOTO) {
-                updateAvatar();
-            }
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        /*if (requestCode == I.REQUEST_CODE_UPDATE_NICK) {
+            initData();
+        }*/
+        osal.setAvatar(requestCode, data, ivPersonalAvatar);
+        if (requestCode == OnSetAvatarListener.REQUEST_CROP_PHOTO) {
+            updateAvatar();
+        }
 //        }
     }
 
@@ -135,11 +212,11 @@ public class PersonalActivity extends BaseActivity {
                     CommonUtils.showLongToast("头像更新失败");
                 } else {
                     User u = (User) result.getRetData();
-                    if(result.isRetMsg()){
+                    if (result.isRetMsg()) {
                         FuLiCenterApplication.setUser(u);
-                        ImageLoader.setAvatar(ImageLoader.getAvatarUrl(u),mContext,ivPersonalAvatar);
+                        ImageLoader.setAvatar(ImageLoader.getAvatarUrl(u), mContext, ivPersonalAvatar);
                         CommonUtils.showLongToast("头像更新成功");
-                    }else{
+                    } else {
                         CommonUtils.showLongToast("头像更新失败");
                     }
                 }
@@ -150,7 +227,7 @@ public class PersonalActivity extends BaseActivity {
             public void onError(String error) {
                 pd.dismiss();
                 CommonUtils.showLongToast("头像更新失败");
-                L.e("error="+error);
+                L.e("error=" + error);
             }
         });
     }
@@ -169,5 +246,6 @@ public class PersonalActivity extends BaseActivity {
         }
         finish();
     }
+
 
 }
